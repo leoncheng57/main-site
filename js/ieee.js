f563938179e99c066875898f9bee9f8d27765b09
cwillaim <cwwomack@mit.edu>
2016-04-11 01:09:16 -0400

    center images in blog post

--- js/ieee.js
+++ js/ieee.js
@@ -19,8 +19,8 @@ $(document).ready(function() {
     $('#blog-post span font').contents().unwrap();
     $('#blog-post span[style]').removeAttr('style');
     $('#blog-post a[style]').removeAttr('style');
-
-    $('#blog-post div img').addClass('center-align z-depth-1');
+    
+    $('#blog-post div img').addClass('center-align center-block z-depth-1');
 
     //Hide CuteNews accreditation (may be sliiiiiightly illegal)
     $('a[href="http://cutephp.com/"]').parent().hide();

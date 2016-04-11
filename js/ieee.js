d2cdc6a480e63dbde12a70a0056306c1aa72b8ed
cwillaim <cwwomack@mit.edu>
2016-04-11 01:18:18 -0400

    add padding around images

--- js/ieee.js
+++ js/ieee.js
@@ -19,8 +19,10 @@ $(document).ready(function() {
     $('#blog-post span font').contents().unwrap();
     $('#blog-post span[style]').removeAttr('style');
     $('#blog-post a[style]').removeAttr('style');
-    
-    $('#blog-post div img').addClass('center-align center-block z-depth-1');
+
+    $('#blog-post div img').addClass('center-block z-depth-1');
+    $('#blog-post div img[style]').removeAttr('style');
+    $('#blog-post div img').parent().addClass('blog-image-padding');
 
     //Hide CuteNews accreditation (may be sliiiiiightly illegal)
     $('a[href="http://cutephp.com/"]').parent().hide();

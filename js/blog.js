363fa312a3f24cca2c95b562cfaedbecb5b8ebf7
cwillaim <cwwomack@mit.edu>
2016-04-13 18:17:20 -0400

    add temporary link wrapper for disabling prev and next links

--- js/blog.js
+++ js/blog.js
@@ -1,17 +1,20 @@
 $(document).ready(function() {
-    $('.cn-current-page-news', '.pagination').wrap("<li class='active'><a></a></li>");
-    $('.cn-current-page-news', '.pagination').contents().unwrap();
+    $('.cn-current-page-news', '.pagination').wrap("<li class='active'></li>");
     $('.cn-page-news', '.pagination').wrap("<li class='waves-effect'></li>");
     var nextEnabled = $('.cn-next-news', '.pagination');
     var prevEnabled = $('.cn-previous-news', '.pagination');
     if (!nextEnabled.length) {
+        $('.next-page i', '.pagination').wrap("<a class='link-wrapper'></a>");
         $('.next-page').addClass('disabled');
     } else {
+        $('.link-wrapper', '.next-page').contents().unwrap();
         $('.next-page').removeClass('disabled');
     }
     if (!prevEnabled.length) {
+        $('.prev-page i', '.pagination').wrap("<a class='link-wrapper'></a>");
         $('.prev-page').addClass('disabled');
     } else {
+        $('.link-wrapper', '.prev-page').contents().unwrap();
         $('.prev-page').removeClass('disabled');
     }
 });

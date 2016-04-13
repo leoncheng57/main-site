eb880662e1361185f9788d2349cc9d0f0ac18930
cwillaim <cwwomack@mit.edu>
2016-04-13 18:01:19 -0400

    current page should be wrapped in 'a' tag

--- js/blog.js
+++ js/blog.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $('.cn-current-page-news', '.pagination').wrap("<li class='active'><a></a></li>");
+    $('.cn-current-page-news', '.pagination').contents().unwrap();
     $('.cn-page-news', '.pagination').wrap("<li class='waves-effect'></li>");
     var nextEnabled = $('.cn-next-news', '.pagination');
     var prevEnabled = $('.cn-previous-news', '.pagination');

5857c7fe0aa468eb32ace43223e8f9ae4052d25e
cwillaim <cwwomack@mit.edu>
2016-06-25 19:27:29 -0400

    fight cutenews with jquery

--- js/ieee.js
+++ js/ieee.js
@@ -10,11 +10,13 @@ $(document).ready(function() {
     $('span font', '.post').contents().unwrap();
     $('span[style]', '.post').removeAttr('style');
     $('a[style]', '.post').removeAttr('style');
-    $('div', '.post').addClass('flow-text');
-    $('p', '.post').addClass('flow-text');
 
     $('div img', '.post').addClass('center-block z-depth-1');
     $('div img', '.post').parent().addClass('blog-image-padding');
+
+    $('.post-title a', '.post').addClass('post-link');
+
+    $('div span', '.post').replaceWith('<p>');
 
     //Hide CuteNews accreditation (may be sliiiiiightly illegal)
     $('a[href="http://cutephp.com/"]').parent().hide();

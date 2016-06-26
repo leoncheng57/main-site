02f2b03b35717374c9d5b4d9604a577bfc76d141
cwillaim <cwwomack@mit.edu>
2016-06-25 23:42:52 -0400

    a couple of minor style changes

--- index.php
+++ index.php
@@ -143,6 +143,7 @@
           $only_active = TRUE;
           include("blog/show_news.php");
         ?>
+        <h5 class="right"><a href="blog.php">All Posts</a></h5>
       </div>
       <div class="divider col s12 hide-on-small-only"></div>
     </div>

ec36e9d461b6a829c119e74b5e65e3ad835ab797
cwillaim <cwwomack@mit.edu>
2016-06-26 02:08:18 -0400

    hide broken images

--- js/blog.js
+++ js/blog.js
@@ -28,4 +28,7 @@ $(document).ready(function() {
     $('.cn-next-news').each(reformHref);
     $('.cn-previous-news').each(reformHref);
 
+    $("img").error(function(){
+        $(this).hide();
+    });
 });

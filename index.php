66f668053ac423b452dbb2b6b1d6f6041a5f4415
Leon Cheng <leonc@mit.edu>
2017-02-17 17:49:39 -0500

    link to / not /club

--- index.php
+++ index.php
@@ -32,14 +32,14 @@
 	<!-- Header -->
   <nav class="z-depth-0">
     <div class="nav-wrapper">
-      <a href="/club/" class="brand-logo"><img class="logo hide-on-med-and-down" src="images/logo-white.png"></a>
+      <a href="/" class="brand-logo"><img class="logo hide-on-med-and-down" src="images/logo-white.png"></a>
       <a href="#" data-activates="mobile-nav" class="button-collapse"><i class="material-icons">menu</i></a>
       <ul class="right hide-on-med-and-down">
-        <li><a href="/club/">HOME</a></li>
+        <li><a href="/">HOME</a></li>
         <li><a href="blog.php">BLOG</a></li>
       </ul>
       <ul class="side-nav" id="mobile-nav">
-        <li><a href="/club/">HOME</a></li>
+        <li><a href="/">HOME</a></li>
         <li><a href="blog.php">BLOG</a></li>
       </ul>
     </div>
